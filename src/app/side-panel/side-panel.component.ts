0d5e9488cc23c00e157a50c25559f83a20c40dc7
tapiomattila <tapiomattila193@gmail.com>
2019-02-13 15:53:17 +0200

    Initial commit, with basic layout

---
+++ src/app/side-panel/side-panel.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-sidepanel',
+    templateUrl: 'side-panel.component.html',
+    styleUrls: ['./side-panel.component.scss']
+})
+
+export class SidePanelComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
