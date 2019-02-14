213e9cffcd1059d4a49976dc39ab53ea27fd255c
tapiomattila <tapiomattila193@gmail.com>
2019-02-14 15:34:58 +0200

    outsourcing items to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from './header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { SidePanelSearchComponent } from './sidepanel-search/sidepanel-search.component';
 import { SidepanelContentComponent } from './sidepanel-content/sidepanel-content.component';
+import { ListCategoryItemComponent } from './sidepanel-content/list-category-item/list-category-item.component';
+import { TodoListItemComponent } from './main-content/todo-list-item/todo-list-item.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { SidepanelContentComponent } from './sidepanel-content/sidepanel-content
     MainContentComponent,
     SidePanelSearchComponent,
     SidepanelContentComponent,
+    ListCategoryItemComponent,
+    TodoListItemComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/sidepanel-content/list-category-item/list-category-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-category-item',
+  templateUrl: './list-category-item.component.html',
+  styleUrls: ['./list-category-item.component.scss']
+})
+export class ListCategoryItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
